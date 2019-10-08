2dd5d86cadc3ffc7dd18e46425e8653d786a272d
Devany <devanydc>
2019-10-08 22:16:37 +0700

    TentangFragment (error)

--- app/src/main/java/com/example/midproject/MainActivity.java
+++ app/src/main/java/com/example/midproject/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.midproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TentangFragment tentangFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,12 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Pizza(View view) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstante) {
+        View view= inflater.inflate(R.layout.fragment_tentang, container, false);
+        return view;
     }
 
-    public void Soda(View view) {
+    public void IceCream(View view) {
     }
 
-    public void EsKrim(View view) {
+    public void HotDrink(View view) {
+    }
+
+    public void ColdDrink(View view) {
     }
 }
